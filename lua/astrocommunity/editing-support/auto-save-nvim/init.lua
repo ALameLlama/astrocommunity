9f23947d7fdd49b5e9476c8b0ab622969b3d633b
Nicholas Ciechanowski <nicholas@ciech.anow.ski>
2026-07-16 09:29:26 +1000

    fix(auto-save-nvim): auto saving in fyler

--- lua/astrocommunity/editing-support/auto-save-nvim/init.lua
+++ lua/astrocommunity/editing-support/auto-save-nvim/init.lua
@@ -50,6 +50,7 @@ return {
     condition = function(buf)
       if vim.tbl_contains({
         "Fyler",
+        "fyler_finder",
       }, vim.fn.getbufvar(buf, "&filetype")) then return false end
 
       return true
